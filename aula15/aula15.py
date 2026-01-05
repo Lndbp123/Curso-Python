18a848593a1fa07da2764c807cdf9d92f3e5009f
Lndbp123 <paixoleonardo96@gmail.com>
2026-01-05 16:10:54 -0300

    input_dados

---
+++ aula15/aula15.py
@@ -0,0 +1,8 @@
+nome = input('Qual o seu nome? ')
+print(f'Olá, {nome}! Seja bem-vindo(a)!')
+
+
+numero_1 = int(input('Digite um número inteiro: '))
+numero_2 = int(input('Digite outro número inteiro: '))  
+
+print (f'A soma dos numeros é {numero_1 + numero_2} ')
