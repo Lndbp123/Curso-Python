954b2f3962b9bbbf4313667512aa47b58ea450e0
Lndbp123 <paixoleonardo96@gmail.com>
2026-01-06 14:20:54 -0300

    operadores

---
+++ aula19/aula19.py
@@ -0,0 +1,33 @@
+"""
+Operadores de comparação (ou relacionais)
+==  igual a
+!=  diferente de    
+>   maior que
+>=  maior ou igual a    
+<   menor que
+<=  menor ou igual a
+Esses operadores sempre retornam um valor booleano (True ou False)
+Exemplos:
+print(1 == 1)      # True
+print(1 != 2)      # True       
+print(3 > 2)       # True
+print(3 >= 3)      # True
+print(2 < 3)       # True
+print(2 <= 2)      # True
+print(1 == 2)      # False
+
+
+
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2   
+menor_ou_igual = 2 <= 2
+igual = 2 == 2
+diferente = 2 != 1
+print(maior)
+print(maior_ou_igual)       
+print(menor)
+print(menor_ou_igual)
+print(igual)
+print(diferente)
