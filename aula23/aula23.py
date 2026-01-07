abe1d7248970c5663406ae2ed639075db843ead2
Lndbp123 <paixoleonardo96@gmail.com>
2026-01-07 16:11:21 -0300

    atualização do curso

---
+++ aula23/aula23.py
@@ -0,0 +1,11 @@
+# Operador logico "not"
+#Usando para inverter o valor de uma expressao booleana
+#not True  # False
+#not False # True
+
+senha = input('Senha: ')
+
+if senha == '123456':
+    print('Acesso permitido!')
+else:
+    print('Acesso negado!')
---
+++ aula24/aula24.py
@@ -0,0 +1,16 @@
+#strings são imutáveis
+# 0 1 2 3 4 5 6 7 8 9
+# P Y T H O N   3 . 86
+#-10-9-8-7-6-5-4-3-2-1
+
+npme = 'Python 3.86'
+print(npme[0])    #Print(npme[1])    #Y
+print(npme[1])    #T        
+print(npme[2])    #H
+print(npme[3])    #O
+print(npme[4])    #espaço
+print(npme[5])    #3
+print(npme[6])    #.        
+print(npme[7])    #8
+print(npme[8])    #6            
+print(npme[9])   #6
